e230e62c11741138081ebcfe5d2f5fd846875882
XIAORAN XIE <59595778+Makiato1999@users.noreply.github.com>
2022-03-31 21:49:44 -0400

    add AllUnitTests, modified business unit tests println

---
+++ flight/app/src/test/java/com/flight/AllUnitTests.java
@@ -0,0 +1,24 @@
+package com.flight;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import com.flight.business.TestFlightInfo;
+import com.flight.business.TestFlightSearch;
+import com.flight.business.TestFlightTable;
+import com.flight.business.TestSearchCityCode;
+import com.flight.business.TestSearchHandler;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        TestFlightInfo.class,
+        // note: only run with hsqldb running on pc
+        // refer to README_HOWTORUN.txt in the main directory
+        //TestFlightSearch.class,
+        TestFlightTable.class,
+        TestSearchCityCode.class,
+        TestSearchHandler.class
+})
+
+public class AllUnitTests {
+}
