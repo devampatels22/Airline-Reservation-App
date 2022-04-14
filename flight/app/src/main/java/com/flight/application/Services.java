03c7e18a2583aafe728c6c8e3229a36ef1df2110
Jordon Dao <daoj@myumanitoba.ca>
2022-04-14 15:57:59 -0500

    Added Reservation Table and related classes/methods

--- flight/app/src/main/java/com/flight/application/Services.java
+++ flight/app/src/main/java/com/flight/application/Services.java
@@ -4,9 +4,12 @@ import com.flight.persistence.CityCodesArray;
 import com.flight.persistence.FakeDB;
 import com.flight.persistence.FlightHandler;
 import com.flight.persistence.IHsqldbFlights;
+import com.flight.persistence.IHsqldbReservations;
 import com.flight.persistence.hsqldb.FlightPersistenceHSQLDB;
+import com.flight.persistence.hsqldb.ReservationPersistenceHSQLDB;
 
 public class Services {
+    private static IHsqldbReservations rp = null;
     private static IHsqldbFlights fp = null;
     private static FlightHandler fh = null;
     private static FakeDB fakeDBPersistence = null;
@@ -27,6 +30,16 @@ public class Services {
         }
 
         return fp;
+    }
+
+    public static synchronized IHsqldbReservations getReservationPersistence()
+    {
+        if (rp == null)
+        {
+            rp = new ReservationPersistenceHSQLDB(Main.getDBPathName());
+        }
+
+        return rp;
     }
 
     public static synchronized FakeDB getFakeDBPersistence()
