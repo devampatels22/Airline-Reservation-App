1460f1e54e5e98c6d67173a21fc162eb59923fb5
Yuhan Shao <603245498@qq.com>
2022-04-18 16:45:19 +0800

    create integration tests and keep adding test in system tests

---
+++ flight/app/src/test/java/com/flight/IntegrationTests.java
@@ -0,0 +1,13 @@
+package com.flight;
+import com.flight.business.AccessCityCodeIT;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        AccessCityCodeIT.class
+})
+public class IntegrationTests {
+
+}
---
+++ flight/app/src/test/java/com/flight/business/AccessCityCodeIT.java
@@ -0,0 +1,52 @@
+package com.flight.business;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import com.flight.persistence.CityCode;
+import com.flight.utils.TestUtils;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public class AccessCityCodeIT {
+    private AccessCityCode accessCityCode;
+    private File tempDB;
+
+
+    @Before
+    public void setUp() throws IOException {
+        // this.tempDB = TestUtils.copyDB();
+        // final CoursePersistence persistence = new CoursePersistenceHSQLDB(this.tempDB.getAbsolutePath().replace(".script", ""));
+        this.accessCityCode = new AccessCityCode();
+    }
+
+    @Test
+    public void testGetCityStrArr() {
+        String[] stringArray = accessCityCode.getCityStrArr();
+        assertNotNull("first sequential course should not be null", stringArray[0]);
+        assertTrue("Calgary YYC".equals(stringArray[0]));
+        assertEquals(13, stringArray.length);
+    }
+
+    @Test
+    public void testGetCityObjArr() {
+        CityCode[] objArray = accessCityCode.getCityObjArr();
+        assertNotNull("first sequential course should not be null", objArray[0]);
+        assertTrue("YYC".equals(objArray[0].getCode()));
+        assertTrue("Calgary".equals(objArray[0].getName()));
+        assertEquals(13, objArray.length);
+    }
+
+//    @After
+//    public void tearDown() {
+//        // reset DB
+//        this.tempDB.delete();
+//    }
+}
