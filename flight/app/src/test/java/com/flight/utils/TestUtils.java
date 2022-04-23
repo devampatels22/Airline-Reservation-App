749d9b8b0c7ab4430cc4827b12f2e5e15efec123
Jordon Dao <daoj@myumanitoba.ca>
2022-04-23 17:46:08 -0500

    use TestUtils in TestFlightSearch

--- flight/app/src/test/java/com/flight/utils/TestUtils.java
+++ flight/app/src/test/java/com/flight/utils/TestUtils.java
@@ -7,11 +7,11 @@ import java.io.File;
 import java.io.IOException;
 
 public class TestUtils {
-    private static final String DB_SRC = "src/main/assets/db/flights.script";
+    private static final File DB_SRC = new File("src/main/assets/db/flights.script");
 
     public static File copyDB() throws IOException {
         final File target = File.createTempFile("temp-db", ".script");
-        Files.copy(new File(DB_SRC), target);
+        Files.copy(DB_SRC, target);
         Main.setDBPathName(target.getAbsolutePath().replace(".script", ""));
         return target;
     }
