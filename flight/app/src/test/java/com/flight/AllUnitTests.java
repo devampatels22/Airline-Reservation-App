9b8fc4f48cb4cd39fa750c753ab15de8c2380ac5
Jordon Dao <daoj@myumanitoba.ca>
2022-04-23 17:55:58 -0500

    refactor names/locations of TestCityCodeStub and TestFlightSearch

--- flight/app/src/test/java/com/flight/AllUnitTests.java
+++ flight/app/src/test/java/com/flight/AllUnitTests.java
@@ -4,7 +4,6 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 import com.flight.business.TestFlightInfo;
-import com.flight.business.TestFlightSearch;
 import com.flight.business.TestFlightTable;
 import com.flight.business.TestSearchCityCode;
 import com.flight.business.TestSearchHandler;
--- flight/app/src/test/java/com/flight/persistence/TestFlightSearch.java
+++ flight/app/src/test/java/com/flight/persistence/TestFlightSearch.java
@@ -3,24 +3,38 @@ package com.flight.persistence;
 import com.flight.application.Services;
 import com.flight.objects.Flight;
 import com.flight.persistence.IHsqldbFlights;
+import com.flight.persistence.hsqldb.FlightPersistenceHSQLDB;
+import com.flight.utils.TestUtils;
 
 import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.IOException;
+
 public class TestFlightSearch {
+    IHsqldbFlights persistence;
+    private File tempDB;
+
     @BeforeClass
     public static void Test_starts() {
         System.out.println("Starting test TestFlightSearch\n");
     }
 
+    @Before
+    public void setUp() throws IOException {
+        this.tempDB = TestUtils.copyDB();
+        persistence = new FlightPersistenceHSQLDB(this.tempDB.getAbsolutePath().replace(".script", ""));
+    }
+
     @Test
     public void searchFlight(){
         System.out.println("Starting searchFlight\n");
-        IHsqldbFlights flightPersist = Services.getFlightPersistence();
-        Flight x = flightPersist.search("YWG","YUL");
+        Flight x = persistence.search("YWG","YUL");
         assert(x.getDepartureCityCode().equals("YWG") && x.getArrivalCityCode().equals("YUL") && x.getDistance() == 1818);
-        x = flightPersist.search("YVR","YYZ");
+        x = persistence.search("YVR","YYZ");
         assert(x.getDepartureCityCode().equals("YVR") && x.getArrivalCityCode().equals("YYZ") && x.getDistance() == 3345);
         System.out.println("Finished searchFlight\n");
     }
@@ -28,9 +42,8 @@ public class TestFlightSearch {
     @Test
     public void returnNull(){
         System.out.println("Starting returnNull\n");
-        IHsqldbFlights flightPersist = Services.getFlightPersistence();
         Flight x = null;
-        x = flightPersist.search("NOT","HERE");
+        x = persistence.search("NOT","HERE");
         assert(x == null);
         System.out.println("Finished returnNull\n");
     }
