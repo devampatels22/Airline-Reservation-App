59f7e592878a92e23ffda69e8a50ae27976a674d
Yuhan Shao <shaoy347@myumanitoba.ca>
2022-04-23 04:02:43 +0000

    Update flight/app/src/test/java/com/flight/IntegrationTests.java

--- flight/app/src/test/java/com/flight/IntegrationTests.java
+++ flight/app/src/test/java/com/flight/IntegrationTests.java
@@ -6,7 +6,9 @@ import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-        AccessCityCodeIT.class
+        AccessCityCodeIT.class,
+        AccessFlightIT.class,
+        AccessReservationIT.class
 })
 public class IntegrationTests {
 
