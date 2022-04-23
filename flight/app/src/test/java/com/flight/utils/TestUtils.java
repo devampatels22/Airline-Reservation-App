9b4b46da9bbc3ef3448a3c18216590d819754b2a
Yuhan Shao <shaoy347@myumanitoba.ca>
2022-04-23 04:05:48 +0000

    Update flight/app/src/test/java/com/flight/utils/TestUtils.java

--- flight/app/src/test/java/com/flight/utils/TestUtils.java
+++ flight/app/src/test/java/com/flight/utils/TestUtils.java
@@ -1,23 +1,18 @@
 package com.flight.utils;
 import com.flight.application.Main;
 
+import com.google.common.io.Files;
+
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 public class TestUtils {
     private static final String DB_SRC = "src/main/assets/db/flights.script";
 
     public static File copyDB() throws IOException {
-        // FileOutputStream outputStream =new FileOutputStream(new File(newFilePath));
-        Path target = Files.createTempFile("temp-db", ".script");
-        target.toFile().deleteOnExit();
-        // final File target = Files.createTempFile("temp-db", ".script").toFile();
-        Files.copy(Paths.get(DB_SRC), target);
-        Main.setDBPathName(target.toString().replace(".script", ""));
-        return target.toFile();
+        final File target = File.createTempFile("temp-db", ".script");
+        Files.copy(new File(DB_SRC), target);
+        Main.setDBPathName(target.getAbsolutePath().replace(".script", ""));
+        return target;
     }
 }
